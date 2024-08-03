6396c3708bcb2f0e9410b53140e69a315a8076bc
Thiago <tiagohenrriquebuenodeoliveira@gmail.com>
2024-08-03 05:57:37 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
+import {
+  DMSans_400Regular,
+  DMSans_500Medium,
+  DMSans_700Bold,
+  useFonts
+} from '@expo-google-fonts/dm-sans';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'DMSans-400': DMSans_400Regular,
+    'DMSans-500': DMSans_500Medium,
+    'DMSans-700': DMSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Thiago</Text>
+      <Text style={{ fontFamily: 'DMSans-400' }}>Thiago</Text>
+      <Text style={{ fontFamily: 'DMSans-700' }}>Thiago</Text>
       <StatusBar style="auto" />
     </View>
   );
